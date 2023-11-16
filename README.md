48b5796cd010c0f251c4aa21281be1e6ef46a69a
xime233 <ximena233laura@gmail.com>
2023-11-16 16:24:46 -0300

    subiendo mercadoliebre

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <img src="https://forthebadge.com/images/badges/powered-by-electricity.svg" alt="powered electricity"/>
+  <img src="https://forthebadge.com/images/badges/winter-is-coming.svg" alt="winter is coming"/>
+  <img src="https://forthebadge.com/images/badges/uses-badges.svg" alt="badges"/>
+  <img src="https://forthebadge.com/images/badges/compatibility-betamax.svg" alt="betamax"/>
+  <img src="https://forthebadge.com/images/badges/certified-snoop-lion.svg" alt="snoop"/>
+</p>
+
+
+# <div align="center"> **Ejercitación e-comerce de Clases Sincrónicas de Curso "Full Stack" en Digital House** </div> <br>
+
+<p align="center">
+  <img width="500px" src="https://raw.githubusercontent.com/Random003/MercadoLiebre/master/public/images/logo-mercado-liebre.svg" alt="Logo Mercado Liebre"/>
+</p> <br>
+
+
+## <div align="center"> Contenido tratado en las clases sincrónicas: </div> <br>
---
+++ app.js
@@ -0,0 +1,61 @@
+// express
+const express = require("express");
+const app = express();
+
+// para que tome la carpeta public, no se necesita poner la ruta completa sino siguiente de public
+app.use(express.static("public"));
+
+// esto es para que ande el app.post y al mandar el formulario no lo envia nada asiqu PONER
+app.use(express.urlencoded({extended: false}))
+
+// para que la pagina no tire el cant get /
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/cart", (req, res) => {
+    res.sendFile(__dirname + "/views/cart.html");
+});
+
+app.get("/help", (req, res) => {
+    res.sendFile(__dirname + "/views/help.html");
+});
+
+app.get("/oficialStores", (req, res) => {
+    res.sendFile(__dirname + "/views/oficialStores.html");
+});
+
+app.get("/sell", (req, res) => {
+    res.sendFile(__dirname + "/views/sell.html");
+});
+
+app.get("*", (req, res) => {
+    res.status(404).sendFile(__dirname + "/views/404.html");
+});
+
+// para que ande el post 
+app.post("/", (req, res) => {
+    // para que haga el JSON dejar este solamente
+    // res.send(req.body);
+    // para que mande directamente a la pagina principal dejar solo este
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+// app.post("/register", (req, res) => {
+//     res.send(req.body);
+// });
+
+// app.post("/login", (req, res) => {
+//     res.send(req.body);
+// });
+
+// para abrir el listen en el puerto determinado
+app.listen(3000, () => { console.log("Código funcionando y escuchando en el puerto 3000;  igual recorda de colocar -npm run startcode- y despues coloca -localhost/3000- en el navegador por favor.")})
